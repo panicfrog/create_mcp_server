f9096adcf97a645def85a9a6937b6594f577c6bb
叶永平 <xieyongping@xieyongpingdeMacBook-Pro-2.local>
2025-03-06 13:39:24 +0800

    add a tool

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,30 +1,38 @@
 import express from 'express';
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { SSEServerTransport } from '@modelcontextprotocol/sdk/server/sse.js'
+import { z } from 'zod';
 
 const server = new McpServer({
   name: "mcp_server_creator",
   version: "1.0.0"
 });
 
+server.tool(
+  "add",
+  { a: z.number(), b: z.number() },
+  async ({ a, b }) => ({
+    content: [{ type: 'text', text: String(a + b) }]
+  })
+)
+
 const app = express();
 const PORT = 3001;
 
-let transports: Record<string, SSEServerTransport> = {}
+let transport: SSEServerTransport | undefined = undefined;
 
 app.get("/sse", async (_, res) => {
-  const transport = new SSEServerTransport("/messages", res);
-  transports.creator = transport;
-  await server.connect(transport)
+  const _transport = new SSEServerTransport("/messages", res);
+  transport = _transport;
+  await server.connect(_transport)
 })
 
 app.post("/messages", async (req, res) => {
-  let transport = transports.creator;
   if (transport) {
     await transport.handlePostMessage(req, res)
   }
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`server listening on port: ${PORT}`);
 });
